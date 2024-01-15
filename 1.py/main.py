711bec4652cb3d7f53b95358ada5fc0738c5f588
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2024-01-15 18:28:52 +0100

    Update main.py

--- 1.py/main.py
+++ 1.py/main.py
@@ -1,11 +1,13 @@
 # main.py
 from edificio import *
 from estilos import *
-from fabrica_edificios import*
+from fabrica_edificios import *
+
 # Crear instancias de estilos arquitectónicos
 estilo_moderno = Moderno()
 estilo_clasico = Clasico()
 estilo_futurista = Futurista()
+
 # Creación de fábricas
 fabrica_residencial_moderno = CreadorResidencial(Moderno(), numero_habitaciones=3)
 nuevo_edificio_residencial = fabrica_residencial_moderno.crear_edificio()
@@ -17,9 +19,9 @@ fabrica_industrial_futurista = CreadorIndustrial(Futurista(), capacidad_producci
 nuevo_edificio_industrial = fabrica_industrial_futurista.crear_edificio()
 
 # Verificar clonación y atributos
-print(isinstance(nuevo_edificio_residencial, EdificioResidencial))  # True
-print(isinstance(nuevo_edificio_comercial, EdificioComercial))  # True
-print(isinstance(nuevo_edificio_industrial, EdificioIndustrial))  # True
-print(isinstance(nuevo_edificio_residencial.estilo, Moderno))  # True
-print(isinstance(nuevo_edificio_comercial.estilo, Clasico))  # True
-print(isinstance(nuevo_edificio_industrial.estilo, Futurista))  # True
+print(nuevo_edificio_residencial)  # Imprime la instancia directamente
+print(nuevo_edificio_comercial)    # Imprime la instancia directamente
+print(nuevo_edificio_industrial)   # Imprime la instancia directamente
+print(nuevo_edificio_residencial.estilo)  # Imprime la instancia del estilo directamente
+print(nuevo_edificio_comercial.estilo)    # Imprime la instancia del estilo directamente
+print(nuevo_edificio_industrial.estilo)   # Imprime la instancia del estilo directamente
